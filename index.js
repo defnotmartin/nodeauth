31211f9f6e539d9b8166728b977e2b71781960cd
defnotmartin <146203207+defnotmartin@users.noreply.github.com>
2023-11-10 02:15:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -6,7 +7,7 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 const compression = require('compression');
 const path = require('path');
-const socketio = require('socket.io');
+
 const jwt = require('jwt-simple');
 
 const apiRouter = require('./routes');
@@ -81,30 +82,31 @@ const expressServer = app.listen(PORT, () => {
   console.log(`Backend listening on port ${PORT}`);
 });
 
-const io = socketio(expressServer);
-app.set('socketio', io);
-console.log('Socket.io listening for connections');
 
-// Authenticate before establishing a socket connection
+// const io = socketio(expressServer);
+// app.set('socketio', io);
+// console.log('Socket.io listening for connections');
 
-//middleware, basically cheking jwt
-io.use((socket, next) => {
-  const token = socket.handshake.query.token;
-  if (token) {
-    try {
-      const user = jwt.decode(token, process.env.JWT_SECRET);
-      if (!user) {
-        return next(new Error('Not authorized.'));
-      }
-      socket.user = user;
-      return next();
-    } catch (err) {
-      next(err);
-    }
-  } else {
-    return next(new Error('Not authorized.'));
-  }
-}).on('connection', (socket) => {
-  socket.join(socket.user.id);
-  console.log('socket connected:', socket.id);
-});
+// // Authenticate before establishing a socket connection
+
+// //middleware, basically cheking jwt
+// io.use((socket, next) => {
+//   const token = socket.handshake.query.token;
+//   if (token) {
+//     try {
+//       const user = jwt.decode(token, process.env.JWT_SECRET);
+//       if (!user) {
+//         return next(new Error('Not authorized.'));
+//       }
+//       socket.user = user;
+//       return next();
+//     } catch (err) {
+//       next(err);
+//     }
+//   } else {
+//     return next(new Error('Not authorized.'));
+//   }
+// }).on('connection', (socket) => {
+//   socket.join(socket.user.id);
+//   console.log('socket connected:', socket.id);
+// });
