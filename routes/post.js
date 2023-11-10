bb18cc2af52e0ff6d085ada29f0f858c00f1062e
defnotmartin <146203207+defnotmartin@users.noreply.github.com>
2023-11-10 19:48:40 +0700

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const postRouter = express.Router();
 const multer = require('multer');
+const storage = multer.memoryStorage();
+
 const upload = multer({
-  dest: 'temp/',
+  storage: storage,
   limits: { fileSize: 10 * 1024 * 1024 },
 }).single('image');
+
 const rateLimit = require('express-rate-limit');
 
 const { requireAuth } = require('../controllers/authController');
